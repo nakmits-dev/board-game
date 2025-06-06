951480942a70b970bea7b75ce7c9e22bcb505e10
NakMits <121592556+nakmits-dev@users.noreply.github.com>
2025-06-06 11:00:57 +0900

    Update both masters

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -64,12 +64,12 @@ export const monsterData: Record<MonsterType, {
 
 export const masterData = {
   player: {
-    name: 'マスター',
+    name: 'ブルーマスター',
     hp: 3,
     attack: 1,
     defense: 1,
     actions: 1,
-    image: 'https://1.bp.blogspot.com/-IS0LxJHJ_Uk/VCOJAcpVgKI/AAAAAAAAm3o/oKxjcjyIgTg/s800/animal_penguin.png',
+    image: 'https://firebasestorage.googleapis.com/v0/b/board-game-5164b.firebasestorage.app/o/cardImg%2F%E3%83%95%E3%82%99%E3%83%AB%E3%83%BC%E3%83%9E%E3%82%B9%E3%82%BF%E3%83%BC.png?alt=media&token=c1d2f5c4-c851-4f5c-9c2c-c3f2d5b0c9f0',
     skills: [{
       name: 'いかりのいちげき',
       description: 'ちかくのてきにこうげき（+1）をあたえる',
